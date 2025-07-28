d553c2145da4bdc891e3afbc98ce94006fc228f5
donghoon11 <100287841+donghoon11@users.noreply.github.com>
2025-07-28 07:30:08 +0000

    Deploying to gh-pages from @ donghoon11/donghoon11.github.io@c6386ca1677fd554a8a27cc6ecfb5450dbb01b51 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-welcome-to-donghoon-oh-s-portfolio",
+    title: "Welcome to Donghoon Oh's Portfolio",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
